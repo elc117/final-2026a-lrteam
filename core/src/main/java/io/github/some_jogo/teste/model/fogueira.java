ef631b5f05b9b1e1a17151b099efa5a11cf15fb0
renata <renata.sfon@gmail.com>
2026-06-13 22:22:10 -0300

    arquivos de classes

---
+++ core/src/main/java/io/github/some_jogo/teste/model/fogueira.java
@@ -0,0 +1,23 @@
+package io.github.some_jogo.teste.model;
+
+public class fogueira extends Estrutura {
+
+    private boolean acesa;
+
+    public Fogueira() {
+        super("Fogueira");
+        this.acesa = false;
+    }
+
+    public void acender() {
+        acesa = true;
+    }
+
+    public void apagar() {
+        acesa = false;
+    }
+
+    public boolean isAcesa() {
+        return acesa;
+    }
+}
---
+++ core/src/main/java/io/github/some_jogo/teste/model/recurso.java
@@ -0,0 +1,26 @@
+package io.github.some_jogo.teste.model;
+
+public abstract class Recurso {
+
+    protected String nome;
+    protected int quantidadeDisponivel;
+
+    public Recurso(String nome, int quantidadeDisponivel) {
+        this.nome = nome;
+        this.quantidadeDisponivel = quantidadeDisponivel;
+    }
+
+    public int coletar(int quantidade) {
+        int coletado = Math.min(quantidade, quantidadeDisponivel);
+        quantidadeDisponivel -= coletado;
+        return coletado;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getQuantidadeDisponivel() {
+        return quantidadeDisponivel;
+    }
+}
