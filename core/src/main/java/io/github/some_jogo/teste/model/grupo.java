e04efca60fce21fc43c46d9e759b0da7f81e4b95
Lauren <lugochlauren@gmail.com>
2026-06-13 21:33:45 -0300

    add class grupo

---
+++ core/src/main/java/io/github/some_jogo/teste/model/grupo.java
@@ -0,0 +1,12 @@
+package io.github.some_jogo.teste.model;
+
+import java.util.ArrayList;
+
+public class Grupo {
+
+    private ArrayList<Personagem> membros;
+
+    public Grupo() {
+        membros = new ArrayList<>();
+    }
+}
