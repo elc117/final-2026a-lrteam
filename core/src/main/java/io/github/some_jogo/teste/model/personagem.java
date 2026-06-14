ae6d277ff9a9db80ddc7b6c508b3e3d45371f712
Lauren <lugochlauren@gmail.com>
2026-06-13 22:23:40 -0300

    add class tarefas e editando

--- core/src/main/java/io/github/some_jogo/teste/model/personagem.java
+++ core/src/main/java/io/github/some_jogo/teste/model/personagem.java
@@ -1,19 +1,20 @@
 package io.github.some_jogo.teste.model;
 
 public class Personagem {
+
     private String nome;
     private String habilidade;
-}
 
-public Personagem(String nome, String habilidade) {
-    this.nome = nome;
-    this.habilidade = habilidade;
-}
+    public Personagem(String nome, String habilidade) {
+        this.nome = nome;
+        this.habilidade = habilidade;
+    }
 
-public String getNome() {
-    return nome;
-}
+    public String getNome() {
+        return nome;
+    }
 
-public String getHabilidade() {
-    return habilidade;
+    public String getHabilidade() {
+        return habilidade;
+    }
 }
---
+++ core/src/main/java/io/github/some_jogo/teste/model/tarefa.java
@@ -0,0 +1,6 @@
+package io.github.some_jogo.teste.model;
+
+public class tarefa {
+    private String nome;
+    private String habilidadeNecessaria;
+}
